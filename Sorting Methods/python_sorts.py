b6d0358143df75a47a2be2c7d46fd7cb342b7699
rmccormick314 <rmccormick314@gmail.com>
2022-01-15 18:02:14 -0700

    Update python_sorts.py

--- Sorting Methods/python_sorts.py
+++ Sorting Methods/python_sorts.py
@@ -61,6 +61,40 @@ def merge_sort(array):
             j += 1
             k += 1
 
+def partition(array, start, end):
+    pivot = array[end // 2]
+
+    i = (start - 1)
+    j = start
+
+    while (j <= end):
+        if (array[j] < pivot):
+            i += 1
+            swap_elements(array, i, j)
+        j += 1
+
+    swap_elements(array, i+1, end)
+    return(i + 1)
+
+def quick_sort(array, start, end):
+
+    if (start < end):
+        partition_index = partition(array, start, end)
+
+        quick_sort(array, start, partition_index-1)
+        quick_sort(array, partition_index+1, end)
+
+def bubble_sort(array):
+    i = len(array)
+    j = k = 0
+
+    while(j < i):
+        while (k < (i - j - 1)):
+            if (array[k] > array[k+1]):
+                swap_elements(array, k, k+1)
+            k += 1
+        j += 1
+
 def test_sorts():
     array = [6, 3, 1, 2, 4, 5, 12, 69, 420, 7]
     print("Base Array:")
@@ -84,5 +118,18 @@ def test_sorts():
     print("Merge Sort:")
     print(test_array_merge)
     print()
+
+    test_array_quick = array
+    quick_sort(test_array_quick, 0, len(test_array_quick)-1)
+    print("Quick Sort:")
+    print(test_array_quick)
+    print()
+
+    test_array_bubble = array
+    bubble_sort(test_array_bubble)
+    print("Bubble Sort:")
+    print(test_array_bubble)
+    print()
+
 
 test_sorts()
